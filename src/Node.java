a37b74b3388a71cf5ebdb86bb11ef5616eda5191
BernigaChristian <christian.berniga@itisondrio.org>
2022-05-09 17:15:57 +0200

    new

--- src/Node.java
+++ src/Node.java
@@ -1,31 +1,31 @@
 public class Node {
-	private Object info;
-	private Node link;
+    private Object info;
+    private Node link;
 
-	public Node(Object o) {
-		info = o;
-		link=null;
-	}
+    public Node(Object o) {
+        info = o;
+        link=null;
+    }
 
-	public Object getInfo() {
-		return info;
-	}
+    public Object getInfo() {
+        return info;
+    }
 
-	public void setInfo(Object info) {
-		this.info = info;
-	}
+    public void setInfo(Object info) {
+        this.info = info;
+    }
 
-	public Node getLink() {
-		return link;
-	}
+    public Node getLink() {
+        return link;
+    }
 
-	public void setLink(Node link) {
-		this.link = link;
-	}
+    public void setLink(Node link) {
+        this.link = link;
+    }
 
-	@Override
-	public String toString(){
+    @Override
+    public String toString(){
         return "[" + info+ "]";
-    }	
+    }
 
 }
